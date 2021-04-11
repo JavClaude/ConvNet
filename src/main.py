bfee8f0614fcbe6fb4279e9e462b62787d41b1fa
Pierre-Arthur <pierre.arthur.claud@outlook.fr>
2021-04-11 21:10:36 +0200

    Add main function

---
+++ src/main.py
@@ -0,0 +1,71 @@
+import logging
+import argparse
+
+import tqdm
+import torch
+
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+from Model.baseline import ConvNet
+from Training.train_eval import train_model, eval_model
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def main(**kwargs) -> None:
+
+    train_dataset = MNIST(
+        root=".",
+        train = True,
+        download=True, 
+        transform = Compose(
+           [
+               ToTensor(),
+               Normalize((0.1307,), (0.3081,))
+               
+           ]
+        )
+    )
+    train_sampler = RandomSampler(train_dataset)
+    train_loader = DataLoader(train_dataset, kwargs["batch_size"], sampler = train_sampler)
+
+
+    test_dataset = MNIST(
+        root=".",
+        train = False,
+        download=True,
+        transform = Compose(
+           [
+                ToTensor(),
+                Normalize((0.1307,), (0.3081,))
+           ]
+        )
+    )
+    test_sampler = SequentialSampler(test_dataset)
+    test_loader = DataLoader(test_dataset, kwargs["batch_size"], sampler = test_sampler)
+
+    model = ConvNet(1, 10)
+    criterion = CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr = kwargs["learning_rate"])
+
+    for _ in tqdm.tqdm(range(kwargs["epochs"])):
+        train_model(model, train_loader, criterion, optimizer)
+        eval_model(model, test_loader, criterion)
+
+if __name__ == "__main__":
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("--epochs", type=int, required=False, default=2)
+    argument_parser.add_argument("--batch_size", type=int, required=False, default=128)
+    argument_parser.add_argument("--learning_rate", type=float, required=False, default=0.0001)
+
+    arguments = argument_parser.parse_args()
+
+    main(**vars(arguments))
